de7678918b3eeb003c1acd29f0674221a5c438a5
Ninjaura314 <jonathanhoyle314@gmail.com>
2019-12-01 23:34:18 -0500

    Added fixed version of Day 1, look I can't remember everything I coded

--- Day1.py
+++ Day1.py
@@ -1,5 +1,5 @@
 with open('day1input.txt') as f:
-    lines = [int(l) for l in f.read().split('\n')]
+    lines = [int(l) for l in f.read().split('\n')[:-1]]
 def get_fuel(n):
     return int(n/3)-2
 part = input('Part 1 or 2?: ')
@@ -7,8 +7,8 @@ ls = []
 for l in lines:
     ls.append(get_fuel(l))
     if part == '2':
-        cf = get_fuel(l)
+        cf = get_fuel(get_fuel(l))
         while cf > 0:
-            ls.append(get_fuel(cf))
+            ls.append(cf)
             cf = get_fuel(cf)
 print(f'Sum of all fuel is {sum(ls)}')
