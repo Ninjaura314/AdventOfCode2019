ee5af472ae9e518f84699c410f11a7e96dddc35b
Ninjaura314 <jonathanhoyle314@gmail.com>
2019-12-03 18:24:34 -0500

    Updating day3.py to be properly formatted and named

--- day3.py
+++ day3.py
@@ -18,47 +18,50 @@ def move(w,d):
     wire_points[w].append(next_point)
     current_points[w] = next_point
 def between_points(p1, p2):
-    bp = []
+    b_points = []
     if p1[0] == p2[0]:
         for i in range(1,abs(p2[1]-p1[1])):
             if p2[1] > p1[1]:
-                bp.append([p1[0],p1[1]+i])
+                b_points.append([p1[0],p1[1]+i])
             else:
-                bp.append([p1[0],p1[1]-i])
+                b_points.append([p1[0],p1[1]-i])
     else:
         for i in range(1,abs(p2[0]-p1[0])):
             if p2[0] > p1[0]:
-                bp.append([p1[0]+i,p1[1]])
+                b_points.append([p1[0]+i,p1[1]])
             else:
-                bp.append([p1[0]-i,p1[1]])
-    return bp
+                b_points.append([p1[0]-i,p1[1]])
+    return b_points
 for w in range(2):
     for d in range(len(wire_moves[w])):
         move(w,wire_moves[w][d])
-sbp = []
-for i in range(len(wire_points[1]) - 1):
-    sbp.append(between_points(wire_points[1][i],wire_points[1][i+1]))
 matching_points = []
+points_before = []
 for i in range(len(wire_points[0])-1):
-    cps = [wire_points[0][i],wire_points[0][i+1]]
+    current_point_s = [wire_points[0][i],wire_points[0][i+1]]
     for j in range(len(wire_points[1])-1):
-        nps = [wire_points[1][j],wire_points[1][j+1]]
-        if ((nps[0][0] == nps[1][0]) and (cps[0][0] == cps[1][0])) or ((nps[0][1] == nps[1][1]) and (cps[0][1] == cps[1][1])):
+        other_points = [wire_points[1][j],wire_points[1][j+1]]
+        if ((other_points[0][0] == other_points[1][0]) and (current_point_s[0][0] == current_point_s[1][0])) or \
+        ((other_points[0][1] == other_points[1][1]) and (current_point_s[0][1] == current_point_s[1][1])):
             continue
-        if nps[0][1] == nps[1][1]:
-            if ((nps[0][0] < cps[0][0]) and (nps[1][0] > cps[0][0])) or ((nps[0][0] > cps[0][0]) and (nps[1][0] < cps[0][0])):
-                cpb = between_points(cps[0],cps[1])
-                spb = between_points(nps[0],nps[1])
-                for c in cpb:
-                    if c in spb:
+        if other_points[0][1] == other_points[1][1]:
+            if ((other_points[0][0] < current_point_s[0][0]) and (other_points[1][0] > current_point_s[0][0])) or \
+            ((other_points[0][0] > current_point_s[0][0]) and (other_points[1][0] < current_point_s[0][0])):
+                current_points_between = between_points(current_point_s[0],current_point_s[1])
+                second_points_between = between_points(other_points[0],other_points[1])
+                for c in current_points_between:
+                    if c in second_points_between:
+                        points_before.append([current_point_s[0],other_points[0]])
                         matching_points.append(c)
                         break
-        if nps[0][0] == nps[1][0]:
-            if ((nps[0][1] < cps[0][1]) and (nps[1][1] > cps[0][1])) or ((nps[0][1] > cps[0][1]) and (nps[1][1] < cps[0][1])):
-                cpb = between_points(cps[0],cps[1])
-                spb = between_points(nps[0],nps[1])
-                for c in cpb:
-                    if c in spb:
+        if other_points[0][0] == other_points[1][0]:
+            if ((other_points[0][1] < current_point_s[0][1]) and (other_points[1][1] > current_point_s[0][1])) or \
+            ((other_points[0][1] > current_point_s[0][1]) and (other_points[1][1] < current_point_s[0][1])):
+                current_points_between = between_points(current_point_s[0],current_point_s[1])
+                second_points_between = between_points(other_points[0],other_points[1])
+                for c in current_points_between:
+                    if c in second_points_between:
+                        points_before.append([current_point_s[0],other_points[0]])
                         matching_points.append(c)
                         break
 def abs_sum(p):
@@ -66,4 +69,15 @@ def abs_sum(p):
 m_sums = []
 for mp in matching_points:
     m_sums.append(abs_sum(mp))
-print(f'{min(m_sums)}')
+print(f'Part 1: {min(m_sums)}')
+wire_distances = []
+def get_wire_distance_to(p,fp,w):
+    current_sum = 0
+    index = wire_points[w].index(p)
+    for i in range(index):
+        current_sum += int(wire_moves[w][i][1:])
+    current_sum += abs(fp[0]-p[0]) + abs(fp[1]-p[1])
+    return current_sum
+for i in range(len(matching_points)):
+    wire_distances.append(get_wire_distance_to(points_before[i][0], matching_points[i], 0) + get_wire_distance_to(points_before[i][1], matching_points[i], 1))
+print(f'Part 2: {min(wire_distances)}')
